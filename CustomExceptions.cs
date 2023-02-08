0b81bedab07fee603f07da631912661f8f5b9727
gmarma23 <gmarma99@gmail.com>
2023-02-08 20:46:00 +0200

    Added custom exceptions implementations

--- CustomExceptions.cs
+++ CustomExceptions.cs
@@ -7,4 +7,16 @@ namespace Battleship
         public InvalidShipPlacementException(string message) : base(message) { }
         public InvalidShipPlacementException() : base() { }
     }
+
+    public class InvalidCellException : Exception
+    {
+        public InvalidCellException(string message) : base(message) { }
+        public InvalidCellException() : base() { }
+    }
+
+    public class InvalidShotException : Exception
+    {
+        public InvalidShotException(string message) : base(message) { }
+        public InvalidShotException() : base() { }
+    }
 }
