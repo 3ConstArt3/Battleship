dc34d993abd092c812c1601ceaa19d232aeb476f
gmarma23 <gmarma99@gmail.com>
2023-02-11 01:12:45 +0200

    Added resource

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -193,6 +193,16 @@ namespace Battleship.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap oceanBgnd {
+            get {
+                object obj = ResourceManager.GetObject("oceanBgnd", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ReadyForBattleBtn {
             get {
                 object obj = ResourceManager.GetObject("ReadyForBattleBtn", resourceCulture);
