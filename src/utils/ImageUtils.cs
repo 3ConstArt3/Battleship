019fe6ce3d0761cb7c9b2752df8a4dfd53d9b533
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2023-02-14 19:56:17 +0200

    scrambled SetBitmapAlpha

--- src/utils/ImageUtils.cs
+++ src/utils/ImageUtils.cs
@@ -8,9 +8,9 @@ namespace Battleship.Utils
         public static Bitmap SetBitmapAlpha(Bitmap bmpIn, int alpha)
         {
             Bitmap bmpOut = new Bitmap(bmpIn.Width, bmpIn.Height);
-            float a = alpha / 255f;
             Rectangle r = new Rectangle(0, 0, bmpIn.Width, bmpIn.Height);
 
+            float a = alpha / 255f;
             float[][] matrixItems = {
                 new float[] {1, 0, 0, 0, 0},
                 new float[] {0, 1, 0, 0, 0},
@@ -18,13 +18,13 @@ namespace Battleship.Utils
                 new float[] {0, 0, 0, a, 0},
                 new float[] {0, 0, 0, 0, 1}};
 
-            ColorMatrix colorMatrix = new ColorMatrix(matrixItems);
-
             ImageAttributes imageAtt = new ImageAttributes();
-            imageAtt.SetColorMatrix(colorMatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            imageAtt.SetColorMatrix(
+                new ColorMatrix(matrixItems), ColorMatrixFlag.Default, ColorAdjustType.Bitmap
+            );
 
-            using (Graphics g = Graphics.FromImage(bmpOut))
-                g.DrawImage(bmpIn, r, r.X, r.Y, r.Width, r.Height, GraphicsUnit.Pixel, imageAtt);
+            Graphics g = Graphics.FromImage(bmpOut);
+            g.DrawImage(bmpIn, r, r.X, r.Y, r.Width, r.Height, GraphicsUnit.Pixel, imageAtt);
 
             return bmpOut;
         }
